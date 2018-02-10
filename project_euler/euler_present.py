ffc6f8fb646841e4459835474ea4b32f4175d851
Adil Iqbal <aiqbal85@gmail.com>
2018-02-09 22:59:41 -0600

    first commit from xubuntu.

---
+++ project_euler/euler_present.py
@@ -0,0 +1,80 @@
+import time
+import signal
+from math import log10, floor
+
+timeout = 90
+
+def handler(signum, frame):
+    """Raise exception if script runs too long."""
+    msg = "SCRIPT TIMED OUT!!!\n More than " + str(timeout) + " seconds have elapsed."
+    raise Exception(msg)
+
+
+def round_sig(x, sig=4):
+    """Round to significant figures."""
+    return round(x, sig-int(floor(log10(abs(x))))-1)
+
+signal.signal(signal.SIGALRM, handler)
+signal.alarm(timeout)
+start = time.time()
+
+# ------------------------------------------ SOLUTIONS BEGINS ------------------------------------------ #
+
+max_length = 0
+best_starting_num = None
+
+# # Brute Force Solution with Array
+# for num in range(1, 1000001):
+# 	collatz_sequence = []
+# 	while num > 1:
+# 		collatz_sequence.append(num)
+# 		if num % 2 == 0:
+# 			num //= 2
+# 		else:
+# 			num = 3 * num + 1
+# 	collatz_sequence.append(1)
+# 	if len(collatz_sequence) > max_length:
+# 		max_length = len(collatz_sequence)
+# 		best_starting_num = collatz_sequence[0]
+
+# # Brute Force Solution with Integers only.
+# for num in range(1, 1000001):
+# 	starting_num = int(num)
+# 	collatz_length = 0
+# 	while num > 1:
+# 		collatz_length += 1
+# 		if num % 2 == 0:
+# 			num //= 2
+# 		else:
+# 			num = 3 * num + 1
+# 	collatz_length += 1
+# 	if collatz_length > max_length:
+# 		max_length = collatz_length
+# 		best_starting_num = starting_num
+
+# Memoized Algorithm!
+stored_lengths = [None]
+for num in range(1, 1000001):
+	starting_num = int(num)
+	collatz_length = 0
+	while num > 1:
+		if len(stored_lengths)-1 >= num:
+			collatz_length = collatz_length + stored_lengths[num]
+			break
+		else:
+			collatz_length += 1
+			if num % 2 == 0:
+				num //= 2
+			else:
+				num = 3 * num + 1
+	if num == 1:
+		collatz_length += 1
+	stored_lengths.append(collatz_length)
+	if collatz_length > max_length:
+		max_length = collatz_length
+		best_starting_num = starting_num
+
+elapsed = time.time() - start
+print("The answer is %s." % best_starting_num)
+print("Solved in %s seconds." % round_sig(elapsed))
+signal.alarm(0)
--- project_euler/problem_012.py
+++ project_euler/problem_012.py
@@ -2,7 +2,7 @@ import time
 import signal
 from math import log10, floor
 
-timeout = 90
+timeout = 20
 
 
 def handler(signum, frame):
@@ -19,39 +19,20 @@ signal.signal(signal.SIGALRM, handler)
 signal.alarm(timeout)
 start = time.time()
 
-'''
-Longest Collatz sequence
-Problem 14
+# ------------ Solution Below this Line ------------
 
-The following iterative sequence is defined for the set of positive integers:
-
-n → n/2 (n is even)
-n → 3n + 1 (n is odd)
-
-Using the rule above and starting with 13, we generate the following sequence:
-
-13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
-It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
-
-Which starting number, under one million, produces the longest chain?
-
-NOTE: Once the chain starts the terms are allowed to go above one million.
-'''
-
-len_dict = {}
+len_dict = []
 
 
 def collatz_generator(n):
     """Generates numbers in the Collatz sequence starting with N. """
     while True:
-        if n <= 1 or n % 1 is not 0:
-            if n == 1:
-                yield n
+        yield n
+        if n <= 1:
             break
         else:
-            yield n
             if n % 2 == 0:
-                n = n//2
+                n = n / 2
             else:
                 n = (3 * n) + 1
 
@@ -67,10 +48,10 @@ def get_collatz_len_of(n):
             try:
                 col_len += len_dict[next_num]
                 raise StopIteration
-            except KeyError:
+            except IndexError:
                 col_len += 1
         except StopIteration:
-            len_dict[n] = col_len
+            len_dict.append(col_len)
             return n, col_len
 
 
